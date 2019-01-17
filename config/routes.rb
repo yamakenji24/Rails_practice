0a9f7065f69725e4412c9ba9d51769cf549ff760
Kenji Yamashita <s16t278@stu.kagawa-u.ac.jp>
2019-01-17 15:41:33 +0900

    Add delete function

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
   get "/" => "home#top"
+  get "posts/:id/edit" => "posts#edit"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "about" => "home#about"
   post "posts/create" => "posts#create"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
+  
 end
