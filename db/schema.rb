b48e9d10669aeb723f020675706cc6a51ac48480
Kenji Yamashita <s16t278@stu.kagawa-u.ac.jp>
2019-01-23 15:01:21 +0900

    Add User information

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_15_061033) do
+ActiveRecord::Schema.define(version: 2019_01_23_053759) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
