4b86b553d1fe56830d9ab30c1cf2f2fbfe000dd0
Kenji Yamashita <s16t278@stu.kagawa-u.ac.jp>
2019-01-24 01:37:40 +0900

    Adding User function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get "signup" => "users#new"
   get 'users/index' => "users#index"
-  
+  get "users/:id" => "users#show"
+  post "users/create" => "users#create"
+  get "users/:id/edit" => "users#edit"
+  post "users/:id/update" => "users#update"
   
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
